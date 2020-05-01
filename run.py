c35c6c43bdf8d6d5faea458a9f07f1606706dd47
Evgeny Krivosheev <K1r9i9v3osheev>
2020-05-01 14:17:41 +0200

    update run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ __author__ = 'JasonLee'
 import commands
 import sys
 import random
+import pandas as pd
 
 # # Read Args
 # if len(sys.argv) != 4 and len(sys.argv) != 5:
@@ -21,13 +22,25 @@ import random
 #     tasktype = 'categorical'
 
 if __name__ == "__main__":
-    method_file = "methods/c_EM/method.py"
-    answer_file = "./demo_answer_file.csv"
-    result_file = "./demo_result_file.csv "
+    method = "GLAD"
+
+    if method == "MV":
+        method_file = "methods/c_MV/method.py"
+    elif method == "DS":
+        method_file = "methods/c_EM/method.py"
+    elif method == "LFC":
+        method_file = "methods/l_LFCbinary/method.py"
+    elif method == "GLAD":
+        method_file = "methods/c_GLAD/method.py"
+
+    answer_file = "data-soft-target/15. Wiki-attack/crowd_answers/crowd_answers_train_all.csv"
+    result_file = "data-soft-target/15. Wiki-attack/agg_train/train_all_{}.csv".format(method)
 
     tasktype = "categorical"
     output = commands.getoutput("python \"" + method_file + "\" \"" + answer_file + "\" " + tasktype).split('\n')[-1]
     e2lpd = eval(output)
+    num_classes = len(e2lpd[e2lpd.keys()[0]])
+    questions = sorted([int(i) for i in e2lpd.keys()])
 
     e2truth = {}
     for e in e2lpd:
@@ -48,11 +61,28 @@ if __name__ == "__main__":
         else:
             truth = e2lpd[e]
 
-        e2truth[e] = truth
+        e2truth[int(e)] = int(truth)
 
+    # with open(result_file, "w") as f:
+    #     f.write("rev_id,label\n")
+    #     for e in e2truth:
+    #         f.write(str(e) + "," + str(e2truth[e]) + "\n")
+    #
+    df = pd.read_csv("data-soft-target/15. Wiki-attack/train.csv")
+    gold_values = df['label'].values
     with open(result_file, "w") as f:
-        f.write("question,result\n")
-        for e in e2truth:
-            f.write(str(e) + "," + str(e2truth[e]) + "\n")
+        header = "text_id,label,{}_label".format(method)
+        for c in range(num_classes):
+            header += ",{}conf{}".format(method, c)
+        header += "\n"
+        f.write(header)
+
+        # for e in e2truth:
+        for q in questions:
+            line = "{},{},{}".format(q, gold_values[q], e2truth[q])
+            for c in range(num_classes):
+                line += ",{}".format(float(e2lpd[str(q)][str(c)]))
+
+            f.write(line + "\n")
 
     print "Finished!, ", method_file
